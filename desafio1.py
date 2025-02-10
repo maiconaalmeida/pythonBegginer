5c9ebfeedde03b53a9ac956aded36e32308a26a0
Maicon A Almeida <71297283+maiconaalmeida@users.noreply.github.com>
2025-02-09 23:14:01 -0300

    Add files via upload
    ✅ Variáveis e Tipos (int, float, str, bool)
    ✅ Operações matemáticas e conversões de tipo (int(), str(), float())
    ✅ Input e output (print(), input())
    
    📌 Desafio: Crie um programa que peça o nome e idade do usuário e exiba uma mensagem formatada.

---
+++ desafio1.py
@@ -0,0 +1,5 @@
+# Solicita a idade do usuário e converte para número inteiro
+idadeUser = int(input("Qual a sua idade? "))  
+
+# Exibe a idade formatada na tela
+print(f"A idade do usuário é {idadeUser} anos.")  
